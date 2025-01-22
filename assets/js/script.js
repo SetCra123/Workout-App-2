5411f1935816a3fd6eb7b91c5cbc31112f44c572
SetrigeCrawford <setrige.crawford@journalism.cuny.edu>
2025-01-21 22:58:24 -0500

    I changed fetch function to asynchronous function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,51 @@
-const tableBody = document.getElementById('repo-table');
-const fetchButton = document.getElementById('fetch-button');
-const apiKey = 'AIzaSyCTYtNordj6SRPwinuAiE_m-PdKmVJBZQo';
+// const tableBody = document.getElementById('repo-table');
+// const fetchButton = document.getElementById('fetch-button');
+// const apiKey = 'AIzaSyCTYtNordj6SRPwinuAiE_m-PdKmVJBZQo';
 
 
-function getApi() {
-  // fetch request gets a list of all the repos for the node.js organization
-  const requestUrl = 'https://www.googleapis.com/auth/fitness.activity.read/users/dataSources/datasets';
+// function getApi() {
+//   // fetch request gets a list of all the repos for the node.js organization
+//   const requestUrl = 'https://www.googleapis.com/auth/fitness.activity.read/users/dataSources/datasets';
 
-  fetch(requestUrl)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-    });
+//   fetch(requestUrl)
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       console.log(data);
+//     });
 
-}
+// }
 
-fetchButton.addEventListener('click', getApi);
+// fetchButton.addEventListener('click', getApi);
+
+async function fetchFitnessData(apiKey, accessToken) {
+    const url = `https://www.googleapis.com/fitness/v1/users/me/dataSources`;
+  
+    try {
+      const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+          'Authorization': `Bearer ${accessToken}`, // Include OAuth 2.0 token
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status} ${response.statusText}`);
+      }
+  
+      const data = await response.json();
+      console.log('Fitness Data:', data);
+      return data;
+    } catch (error) {
+      console.error('Error fetching fitness data:', error);
+    }
+  }
+  
+  // Usage example:
+  const apiKey = 'AIzaSyCTYtNordj6SRPwinuAiE_m-PdKmVJBZQo'; 
+  const accessToken = 'your-oauth-access-token'; // Must be obtained via the OAuth flow
+  
+  fetchFitnessData(apiKey, accessToken);
+  

ff57f6c29141da8c0f2980fdac48bed27a6e9c35
SetrigeCrawford <setrige.crawford@journalism.cuny.edu>
2025-02-22 20:01:08 -0500

    I added the user model for a user profile

---
+++ server/models/User.js
@@ -0,0 +1,66 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+const bcrypt = require('bcrypt');
+
+
+const userSchema = new Schema (
+    {
+      username: {
+        type: String,
+        required: true,
+      },
+      email: {
+        type: String,
+        required: true,
+      },
+      password: {
+        type: String,
+        required: true,
+      },
+      height: [{
+        type: Schema.Types.ObjectID,
+        ref: "Height",
+        required: true,
+      }],
+      weight: [{
+        type: Schema.Types.ObjectID,
+        ref: "Weight",
+        required: true,
+      }],
+      current_body_type: [{
+        type: Schema.Types.ObjectID,
+        ref: "Curret_Body_Type",
+        required: true,
+      }],
+      goal_body_type: [{
+        type: Schema.Types.ObjectID,
+        ref: "Goal_Body_Type",
+        required: true,
+      }],
+      createdAt: {
+        type: Date,
+        default: Date.now(),
+        require: true,
+      },
+
+ }
+);
+
+//set-up middleware to create password
+userSchema.pre('save', async function (next) {
+    if (this.isNew || this.isModified('password')) {
+      const saltRounds = 10;
+      this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+  
+    next();
+  });
+  
+//check if entered password matches saved password
+userSchema.methods.isCorrectPassword = async function (password) {
+    return bcrypt.compare(password, this.password);
+  };
+
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const path = require('path');
-const db = require('./config/connection');
-// const routes = require('./routes');
+// const path = require('path');
+// const db = require('./config/connection');
+// // const routes = require('./routes');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -13,7 +13,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/dist')));
 }
 
-app.use(routes);
+// app.use(routes);
 
 db.once('open', () => {
     app.listen(PORT, () => console.log(`Now listening on localhost:${PORT}`));
